101a9a0cf1b5b6d5a6a8074a5896d11f93516c61
aminbro <barrimohamed01@gmial.com>
2023-06-23 11:27:24 +0100

    first commit

--- delete-client.php
+++ delete-client.php
@@ -1,7 +1,10 @@
 <?php
     if(isset($_GET['id'])){
         include('connection.php');
-        
+        $ins1 = $pdo->prepare("DELETE FROM `reserver-hotel` WHERE `id-client`=?");
+        $ins1->execute(array($_GET['id']));
+        $ins2 = $pdo->prepare("DELETE FROM `reserver-voyage` WHERE `id-cleint`=?");
+        $ins2->execute(array($_GET['id']));
         $ins = $pdo->prepare("DELETE FROM `client` WHERE `id_client`=?");
         $ins->execute(array($_GET['id']));
         if($ins){
